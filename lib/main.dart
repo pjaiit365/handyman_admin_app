18388aa010ea2f835f61b4c7b3ae6da69e0c8d44
pjaiit365 <pjaiitey@gmail.com>
2023-07-18 02:34:37 +0000

    design 1.0

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,12 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:handyman_admin_app/Screens/Splash/splash_screen.dart';
 
 import 'constants.dart';
 
-void main() {
-  // WidgetsFlutterBinding.ensureInitialized();
-  // await Firebase.initialiseApp();
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
